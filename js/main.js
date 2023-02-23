ac6e85263a8e61d2ad7c1d8cd4a4c6ba8412b80d
Magaradga <125486469+Magaradga@users.noreply.github.com>
2023-02-23 23:40:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,4 +14,33 @@ $(function () {
     $('.home__slader').slick('slickNext')
   })
 
+  $('.auction__slader').slick({
+    arrows:false,
+    dots: false,
+    waitForAnimate: false,
+    slidesToShow: 4,
+  })
+  $('.auction__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.auction__slader').slick('slickPrev')
+  })
+  $('.auction__next').on('click', function (e) {
+    e.preventDefault()
+    $('.auction__slader').slick('slickNext')
+  })
+
+  $('.questions__link').on('click', function(e){
+    e.preventDefault()
+    if ($(this).hasClass('questions__link--active')) {
+        $(this).removeClass('questions__link--active')
+        $(this).children('.questions__text').slideUp()
+    } else {
+        $('.questions__link').removeClass('questions__link--active')
+        $('.questions__text').slideUp()
+        $(this).addClass('questions__link--active')
+        $(this).children('.questions__text').slideDown()
+    }
+})
+
+
 })
