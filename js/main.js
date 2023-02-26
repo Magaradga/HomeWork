0bcf871f388cca2e981e44d0b6c7f270a29cde37
Magaradga <125486469+Magaradga@users.noreply.github.com>
2023-02-26 16:46:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,9 +16,32 @@ $(function () {
 
   $('.auction__slader').slick({
     arrows:false,
-    dots: false,
+    dots: true,
+    appendDots: $('.auction__dots'),
     waitForAnimate: false,
     slidesToShow: 4,
+    responsive:
+        [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    draggable: true,
+                },
+            },
+        ]
   })
   $('.auction__prev').on('click', function (e) {
     e.preventDefault()
@@ -49,19 +72,37 @@ $(document).ready(function() {
 	$('.selection__form--selection, .selection__row').styler({
   });
 
-  $('.burger').on('click', function(e) {
-    e.preventDefault()
-    $('.burger').toggleClass('burger--active');
-  }) 
 
-
-
-    $('.burger, .overlay, .header__inner a').on('click', function(e) {
+    $('.burger, .overlay, .header__inner a[href^="#"').on('click', function(e) {
       e.preventDefault()
       $('.header__inner').toggleClass('header__inner--open')
       $('.overlay').toggleClass('overlay--show')
+      $('.burger').toggleClass('burger--active');
     }) 
     
   
+
+    $('.fmain__contact--span').on('click', function(){
+      $('.fmain__contact--list').slideToggle()
+  })
+  $('.fmain__menu--span').on('click', function(){
+    $('.fmain__menu--list').slideToggle()
+})
+$('.fmain__auto--span').on('click', function(){
+  $('.fmain__auto--list').slideToggle()
+})
+
+$('.home__top--btn--link--burger').on('click', function(){
+  $('.home__top--list--burger').slideToggle()
+})
+
+
+$('a[href^="#"').on('click', function() {
+  let href = $(this).attr('href');
+  $('html, body').animate({
+      scrollTop: $(href).offset().top
+    }, 1000);
+  return false;
+});
 })
 
